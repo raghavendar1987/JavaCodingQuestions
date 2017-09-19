8dae0158f98e37989888ffce34168565bdb6f112
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-20 02:23:34 +0530

    Update PermuteString.java

--- PermuteString.java
+++ PermuteString.java
@@ -1,1 +1,13 @@
-
+// Print all permutation of String both iterative and Recursive way
+import java.util.ArrayList;
+public class PermuteString {
+    public static void main(String args[]) {
+        String str = "kunal";
+        char ar [] = str.toCharArray(); 
+        int l = ar.length();
+        ArrayList<String> al = new ArrayList<String>();
+        for(int i = 0;i<l;i++){
+            
+        }
+    }
+}

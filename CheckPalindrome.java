814563a2928c72d0c08be4874351785dd2291da2
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-20 00:46:24 +0530

    Update CheckPalindrome.java

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -14,28 +14,31 @@ public class CheckPalindrome {
 }
 
 // Palindrome No generic solution
+// Palindrome No generic solution
 public class CheckPalindrome {
     public static void main(String args[]) {
-       String str1 = "noon";
-       String sArray = new String["noon", "redivider", "civic", "radar", "level", "rotor", "kayak"];
-       int len = str1.length();
+       String sArray[] = {"noon", "redivider", "nirala", "civic", "radar", "kishor", "level", "rotor", "kayak", "kunal"};
+       int len = 0;
        Boolean c = false;
      //   String str2 = new String[len];
       for(int j=0;j<sArray.length;j++){
+          c = false;
+          len = sArray[j].length();
            for(int i=0;i<(len/2);i++){
-               if(str1.charAt(i) == str1.charAt(len-i-1))
+               if(sArray[j].charAt(i) == sArray[j].charAt(len-i-1))
                {
                    c = true;
                }
                else{
                     c = false;
-                   System.out.println(sArray[j]+ "No palindrome.");
+                   System.out.println(sArray[j]+ ": No palindrome.");
                    break;
                }
            }
            if(c){
-                System.out.println(sArray[j]+ "palindrome.");
-           }
-        }
+            System.out.println(sArray[j]+ ": palindrome.");
+       }
+      }
+        
     }
 }

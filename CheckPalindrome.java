2822a8b0dcccbdf5e5be61eb92177fa09e0b3b81
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-19 19:24:57 +0530

    Update CheckPalindrome.java

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -1,3 +1,4 @@
+// Palindrome No using reverse method
 public class CheckPalindrome {
     public static void main(String args[]) {
         System.out.println("Code to check a String is palindrome or not");
@@ -7,5 +8,32 @@ public class CheckPalindrome {
             System.out.println("It's palindrome");
         }
         
+    }
+}
+
+// Palindrome No generic solution
+public class CheckPalindrome {
+    public static void main(String args[]) {
+       String str1 = "noon";
+       String sArray = new String["noon", "redivider", "civic", "radar", "level", "rotor", "kayak"];
+       int len = str1.length();
+       Boolean c = false;
+     //   String str2 = new String[len];
+      for(int j=0;j<sArray.length;j++){
+           for(int i=0;i<(len/2);i++){
+               if(str1.charAt(i) == str1.charAt(len-i-1))
+               {
+                   c = true;
+               }
+               else{
+                    c = false;
+                   System.out.println(sArray[j]+ "No palindrome.");
+                   break;
+               }
+           }
+           if(c){
+                System.out.println(sArray[j]+ "palindrome.");
+           }
+        }
     }
 }

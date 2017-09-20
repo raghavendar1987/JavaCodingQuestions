5d86311d1fa193acfbfb819c0a813df1c2e84282
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-20 11:46:40 +0530

    Update PermuteString.java

--- PermuteString.java
+++ PermuteString.java
@@ -1,13 +1,22 @@
+
 // Print all permutation of String both iterative and Recursive way
 import java.util.ArrayList;
 public class PermuteString {
     public static void main(String args[]) {
         String str = "kunal";
-        char ar [] = str.toCharArray(); 
-        int l = ar.length();
+        PermuteString pr = new PermuteString();
         ArrayList<String> al = new ArrayList<String>();
-        for(int i = 0;i<l;i++){
-            
+        for(int i = 0; i<str.length;i++){
+             al.add(pr.findPer(str, i));
         }
+        System.out.println(al);
+    }
+    ArrayList<String> findPer(String str,int i){
+        ArrayList<String> a2 = new ArrayList<String>();
+        char ch [] = str.toCharArray();     
+        return r2;
     }
 }
+
+kunal(k) :- kunal, uknal, unkal, unakl, unalk.
+kunal(u) :- 

62a8103d586b74fa2d4618959cb3a259f9815a67
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-21 01:33:02 +0530

    Update PermuteString.java

--- PermuteString.java
+++ PermuteString.java
@@ -1,22 +1,63 @@
 
-// Print all permutation of String both iterative and Recursive way
+// Print all permutation of String both iterative way
 import java.util.ArrayList;
 public class PermuteString {
     public static void main(String args[]) {
-        String str = "kunal";
+        String str = "ABCD";
         PermuteString pr = new PermuteString();
-        ArrayList<String> al = new ArrayList<String>();
-        for(int i = 0; i<str.length;i++){
-             al.add(pr.findPer(str, i));
+        for(int i = 0; i<str.length();i++){
+             pr.findPer(str, i);
         }
-        System.out.println(al);
     }
-    ArrayList<String> findPer(String str,int i){
-        ArrayList<String> a2 = new ArrayList<String>();
+    static void findPer(String str,int i){
+        ArrayList<String> a1 = new ArrayList<String>();
         char ch [] = str.toCharArray();     
-        return r2;
+        char temp = ch[i];
+        ch[i] = ch[0];
+        ch[0] = temp;
+        String st = new String(ch);
+        for(int k = 1; k<ch.length;k++){
+            for(int j = 1; j<ch.length-1;j++){
+                char t = ch[j];
+                ch[j] = ch[j+1];
+                ch[j+1] = t;
+                String b = new String(ch);
+                a1.add(b);
+            }
+        }
+      System.out.println(a1);
     }
 }
 
-kunal(k) :- kunal, uknal, unkal, unakl, unalk.
-kunal(u) :- 
+// Print all permutation of String Recursive way
+import java.util.ArrayList;
+public class PermuteString {
+    public static void main(String args[]) {
+        String str = "ABCD";
+        PermuteString pr = new PermuteString();
+        pr.findPer(str, 0);
+        
+    }
+    static void findPer(String str,int i){
+        ArrayList<String> a1 = new ArrayList<String>();
+        char ch [] = str.toCharArray();     
+        char temp = ch[i];
+        ch[i] = ch[0];
+        ch[0] = temp;
+        String st = new String(ch);
+        for(int k = 1; k<ch.length;k++){
+            for(int j = 1; j<ch.length-1;j++){
+                char t = ch[j];
+                ch[j] = ch[j+1];
+                ch[j+1] = t;
+                String b = new String(ch);
+                a1.add(b);
+            }
+        }
+      System.out.println(a1);
+      i++;
+      if(i<str.length()){
+          findPer(str, i);
+      }
+    }
+} 

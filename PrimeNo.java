435aadb62c34d2a80acaec232d861344e44273d4
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-21 17:12:38 +0530

    Update PrimeNo.java

--- PrimeNo.java
+++ PrimeNo.java
@@ -1,16 +1,28 @@
-public class PrimeNo {
-    public static void main(String args[]) {
-      int n = 100;
-	  System.out.println("Prime No from 1 to 100.");
-      for(int i=1;i<=n;i++){
-          for(int j =2; j<=i;j++){
-              if(i == j){
-                  System.out.print(" "+ i);
-              }
-              else if(i%j == 0){
-                  break;
-              }
-          }
-      }
-    }
+// 1 to 100 prime No and find max no.
+package com.kunal.Test;
+
+import java.util.ArrayList;
+
+public class Test1 {
+	public static void main(String[] args) {
+		int n = 100;
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for(int i = 2;i<n;i++){
+			for(int j = 2;j<=i;j++){
+				if(i%j != 0){
+					continue;
+				}
+				else if(i == j){
+					al.add(i);
+				}
+				else{
+					break;
+				}
+			}
+			
+		}
+		System.out.println(al);
+		System.out.println("Smallest prime no is: "+ al.get(0));
+		System.out.println("Greatest prime no is: "+ al.get(al.size()-1));
+	}
 }

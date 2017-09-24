1b0b5ae448f57a4ee59dbe2dbd7d8f4fcd6429a1
Kunal Kishor Nirala <kknirala31@gmail.com>
2017-09-25 00:46:56 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,10 @@
+// How to convert numeric String to int  and add some int value in Java?
+
+public class StringToInt {
+    public static void main(String args[]) {
+       String str = "5456454";
+       System.out.println(Integer.parseInt(str));
+       int st = Integer.parseInt(str)+10;
+       System.out.println(st);
+    }
+}
